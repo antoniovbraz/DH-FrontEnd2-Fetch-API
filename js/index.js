8752e3926f6acce08df11f55e97c138d35017212
Antonio Braz <antoniovbraz@gmail.com>
2022-03-30 20:12:48 -0300

    Class A17S

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
 // Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
-fetch('https://randomuser.me/api/')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        //manipulamos a resposta
-        console.log(data)
-    });
+fetch("https://randomuser.me/api/")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    //manipulamos a resposta
+    console.log(data);
+  });
 
 function renderizarDadosUsuario(dados) {
-    /* -------------------------------- Tarefa 1 -------------------------------- */
-    // Aqui devem desenvolver uma função que seja exibida na tela:
-    // a foto, o nome completo do usuário e o e-mail.
-    // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
+  /* -------------------------------- Tarefa 1 -------------------------------- */
+  // Aqui devem desenvolver uma função que seja exibida na tela:
+  // a foto, o nome completo do usuário e o e-mail.
+  // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
 }
-
 
 /* --------------------------- Tarefa 2 (extra) --------------------------- */
 // Aqui você pode ir para o ponto extra de usar o botão que está comentado no HTML.
